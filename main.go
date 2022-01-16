f1fa602346a4cf282a1ab2124246ba4570037b30
danyu rajbahak <rajbahakdanyu@gmail.com>
2022-01-16 15:28:34 +0545

    Updated routes

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "Yellow",
 		})
 	})
 	r.Run()
