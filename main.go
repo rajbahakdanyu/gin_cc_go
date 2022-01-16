177a9b57ea57791d47a03f7bbc5898f2c8d4ea7e
danyu rajbahak <rajbahakdanyu@gmail.com>
2022-01-16 19:05:38 +0545

    Added a book strut

--- main.go
+++ main.go
@@ -2,12 +2,21 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+type book struct {
+	ID       string `json:"id"`
+	Title    string `json:"title"`
+	Author   string `json:"author"`
+	Quantity int    `json:"quantity"`
+}
+
 func main() {
 	r := gin.Default()
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Yellow",
 		})
 	})
-	r.Run()
+
+	r.Run("localhost:8000")
 }
