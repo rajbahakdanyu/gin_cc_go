cccc64fcbdc2c2b8096c6924793dd441f7b780a7
danyu rajbahak <rajbahakdanyu@gmail.com>
2022-01-16 19:06:03 +0545

    Added a books data store

--- main.go
+++ main.go
@@ -9,6 +9,12 @@ type book struct {
 	Quantity int    `json:"quantity"`
 }
 
+var books = []book{
+	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
+	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
+	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
+}
+
 func main() {
 	r := gin.Default()
 
